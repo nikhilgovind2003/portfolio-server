6751964c18af56731386f9e375d8d43ac528b07b
Nikhil Govind OV <84319053+nikhilgovind2003@users.noreply.github.com>
2025-09-28 13:34:53 +0530

    add optional for cms validator

--- validations/cmsValidator.js
+++ validations/cmsValidator.js
@@ -2,19 +2,19 @@ const { body, validationResult } = require("express-validator");
 
 // Validation rules for update
 const updateCmsRules = [
-  body("super_title").isString().withMessage("Super title must be a string"),
-  body("title").isString().withMessage("Title must be a string"),
-  body("description").isString().withMessage("Description must be a string"),
-  body("btn_one_text").isString().withMessage("Button one text must be a string"),
-  body("btn_one_link").isString().withMessage("Button one link must be a string"),
-  body("btn_two_text").isString().withMessage("Button two text must be a string"),
-  body("btn_two_link").isString().withMessage("Button two link must be a string"),
-  body("media_alt").isString().withMessage("Media alt text must be a string"),
-  body("project_title").isString().withMessage("Project title must be a string"),
-  body("skills_title").isString().withMessage("Skills title must be a string"),
-  body("about_title").isString().withMessage("About title must be a string"),
-  body("about_description").isString().withMessage("About description must be a string"),
-  body("contact_title").isString().withMessage("Contact title must be a string"),
+  body("super_title").isString().optional().withMessage("Super title must be a string"),
+  body("title").isString().optional().withMessage("Title must be a string"),
+  body("description").isString().optional().withMessage("Description must be a string"),
+  body("btn_one_text").isString().optional().withMessage("Button one text must be a string"),
+  body("btn_one_link").isString().optional().withMessage("Button one link must be a string"),
+  body("btn_two_text").isString().optional().withMessage("Button two text must be a string"),
+  body("btn_two_link").isString().optional().withMessage("Button two link must be a string"),
+  body("media_alt").isString().optional().withMessage("Media alt text must be a string"),
+  body("project_title").isString().optional().withMessage("Project title must be a string"),
+  body("skills_title").isString().optional().withMessage("Skills title must be a string"),
+  body("about_title").isString().optional().withMessage("About title must be a string"),
+  body("about_description").isString().optional().withMessage("About description must be a string"),
+  body("contact_title").isString().optional().withMessage("Contact title must be a string"),
 ];
 
 // Middleware to check validation results
